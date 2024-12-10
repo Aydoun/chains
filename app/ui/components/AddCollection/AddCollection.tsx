e186104f15975914d1007cf717e7e5ac795fa4ea
amineaydoun <mohamedamineaydoun@pvh.com>
2024-12-10 15:15:14 +0100

    add "add collection" card

--- app/ui/components/AddCollection/AddCollection.tsx
+++ app/ui/components/AddCollection/AddCollection.tsx
@@ -4,15 +4,19 @@ import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function AddCollection() {
   return (
-    <Card className="w-96 shadow-lg border border-gray-200">
-      <CardHeader className="text-center">
-        <CardTitle className="text-xl font-bold text-gray-800">
+    <Card className="w-96 bg-black shadow-xl rounded-xl border border-gray-700 relative overflow-hidden">
+      <CardHeader className="text-center relative z-10">
+        <CardTitle className="text-2xl font-bold text-white">
           Add a Collection
         </CardTitle>
       </CardHeader>
-      <CardFooter className="flex justify-center">
-        <Button variant="outline" size="lg">
-          <CirclePlus />
+      <CardFooter className="flex justify-center relative z-10">
+        <Button
+          size="lg"
+          className="flex items-center space-x-2 bg-pink-500 text-white hover:bg-pink-600"
+        >
+          <CirclePlus className="w-5 h-5" />
+          <span>Add</span>
         </Button>
       </CardFooter>
     </Card>
--- app/ui/components/Header/HeaderComponent.tsx
+++ app/ui/components/Header/HeaderComponent.tsx
@@ -1,13 +1,25 @@
-import { User, Settings } from "lucide-react";
+"use client";
+
+import { useState } from "react";
+import { User, Settings, Sun, Moon } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export default () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
     <header className="bg-white shadow-md p-4 z-10">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         <h1 className="text-lg font-bold text-gray-800">Chains</h1>
         <nav>
           <div className="flex items-center space-x-4">
+            <Button
+              onClick={() => setIsDarkMode((prev) => !prev)}
+              variant="outline"
+              size="icon"
+            >
+              {isDarkMode ? <Moon /> : <Sun />}
+            </Button>
             <Button variant="outline" size="icon">
               <User />
             </Button>
